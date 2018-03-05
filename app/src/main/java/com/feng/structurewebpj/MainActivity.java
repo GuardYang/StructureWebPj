b7ab04f3d34fe0e8396f70838a6f915b1b23099e
feng <yangshirong@log56.com>
2018-03-05 14:11:59 +0800

    Initial commit

---
+++ app/src/main/java/com/feng/structurewebpj/MainActivity.java
@@ -0,0 +1,26 @@
+package com.feng.structurewebpj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient() {
+            //覆盖shouldOverrideUrlLoading 方法
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webview.loadUrl("https://www.baidu.com/");
+    }
+}
